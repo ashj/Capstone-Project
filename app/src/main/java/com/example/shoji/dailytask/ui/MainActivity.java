d43bd6be7b87f89be6b62953be3299f4ec85c12b
lsitec333.asato <andre.shoji@lsitec.org.br>
2018-02-01 09:11:34 -0200

    [ui] main activity general layout
    Signed-off-by: lsitec333.asato <andre.shoji@lsitec.org.br>

--- app/src/main/java/com/example/shoji/dailytask/ui/MainActivity.java
+++ app/src/main/java/com/example/shoji/dailytask/ui/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.shoji.dailytask.ui;
 
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.shoji.dailytask.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton mFab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFab = findViewById(R.id.fab);
+
+        final Context context = this;
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "FAB", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
