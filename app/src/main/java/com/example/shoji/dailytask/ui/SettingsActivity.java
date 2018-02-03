7163a197dea91994584a61d0748ed3aec1283f44
lsitec333.asato <andre.shoji@lsitec.org.br>
2018-02-02 23:19:34 -0200

    [ui] added action bar for settings. fixed parent activity
    Signed-off-by: lsitec333.asato <andre.shoji@lsitec.org.br>

--- app/src/main/java/com/example/shoji/dailytask/ui/SettingsActivity.java
+++ app/src/main/java/com/example/shoji/dailytask/ui/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.shoji.dailytask.ui;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.shoji.dailytask.R;
 
@@ -11,5 +14,23 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // [START] customize action bar
+        getSupportActionBar().setTitle(getString(R.string.menu_settings_label));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // [END] customize action bar
     }
+
+    // [START] customize action bar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    // [END] customize action bar
 }
