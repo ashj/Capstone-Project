ab477cc21d3fa0cdb19f414b789a1555b11452e0
lsitec333.asato <andre.shoji@lsitec.org.br>
2018-02-12 12:25:15 -0200

    tint check marks in jistory based on priority
    Signed-off-by: lsitec333.asato <andre.shoji@lsitec.org.br>

--- app/src/main/java/com/example/shoji/dailytask/adapter/TaskHistoryViewHolder.java
+++ app/src/main/java/com/example/shoji/dailytask/adapter/TaskHistoryViewHolder.java
@@ -2,9 +2,12 @@ package com.example.shoji.dailytask.adapter;
 
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.database.Cursor;
+import android.support.v4.widget.ImageViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.shoji.dailytask.R;
@@ -17,10 +20,13 @@ public class TaskHistoryViewHolder extends RecyclerView.ViewHolder
                  implements View.OnClickListener {
     public static final int RES_LAYOUT_ID = R.layout.view_holder_task_history_list;
     private Context mContext;
+    private ImageView mCheckImage;
     private TextView mTitleTextView;
     private TextView mModifiedDate;
 
     private OnClickListener mOnClickListener;
+
+    private static int[] sColors;
 
     public interface OnClickListener {
         void onClick(int position);
@@ -32,6 +38,7 @@ public class TaskHistoryViewHolder extends RecyclerView.ViewHolder
         View.OnClickListener onClickListener = this;
         itemView.setOnClickListener(onClickListener);
 
+        mCheckImage = itemView.findViewById(R.id.checked_image);
         mTitleTextView = itemView.findViewById(R.id.title_text_view);
         mModifiedDate = itemView.findViewById(R.id.modified_date_text_view);
     }
@@ -50,8 +57,8 @@ public class TaskHistoryViewHolder extends RecyclerView.ViewHolder
 //        columnIndex = cursor.getColumnIndex(TaskContract._ID);
 //        String id = cursor.getString(columnIndex);
 //        columnIndex = cursor.getColumnIndex(TaskContract.COLUMN_PRIORITY);
-//        String priority = cursor.getString(columnIndex);
-//        title = title + " (id:"+id+"//P"+priority+")";
+//        String priorityStr = cursor.getString(columnIndex);
+//        title = title + " (id:"+id+"//P"+priorityStr+")";
 
         mTitleTextView.setText(title);
 
@@ -60,6 +67,23 @@ public class TaskHistoryViewHolder extends RecyclerView.ViewHolder
         String dateStr = TimeUtils.timeInMillisToFormattedString(context, modification_date);
 
         mModifiedDate.setText(dateStr);
+
+        // [START] tint check icon
+        if(sColors == null) {
+            sColors = initColorMap(context);
+        }
+        columnIndex = cursor.getColumnIndex(TaskContract.COLUMN_PRIORITY);
+        int priority = cursor.getInt(columnIndex);
+        if(0 <= priority && priority < sColors.length) {
+            int color = sColors[priority];
+            ImageViewCompat.setImageTintList(mCheckImage,
+                    ColorStateList.valueOf(color));
+        }
+        // [END] tint check icon
+    }
+
+    private int[] initColorMap(Context context) {
+        return context.getResources().getIntArray(R.array.priority_color_array);
     }
 
     @Override
